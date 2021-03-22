796c1387b877e43785f95f05c912cd9808c1066d
amardeep <amardeep.dosanjh@and.digital>
2021-03-22 10:46:57 +0000

    Core setup done - font awesome pending

--- src/App.vue
+++ src/App.vue
@@ -25,5 +25,6 @@ export default Vue.extend({
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  color: $primary-red;
 }
 </style>
--- tests/unit/example.test.ts
+++ tests/unit/example.test.ts
@@ -1,12 +1,14 @@
-import { shallowMount } from "@vue/test-utils";
+import { render, screen } from "@testing-library/vue";
 import HelloWorld from "@/components/HelloWorld.vue";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
+
+    render(HelloWorld, {
       propsData: { msg },
     });
-    expect(wrapper.text()).toMatch(msg);
+
+    expect(screen.getByText(msg)).toBeTruthy();
   });
 });
